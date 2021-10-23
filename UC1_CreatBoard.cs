36dcbc3146e2a01bfa962be01c30e832873732e1
unknown <manaspatil2030@gmail.com>
2021-10-23 09:25:19 +0530

    Added UC1 - Creating Board

---
+++ UC1_CreatBoard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    class UC1
+    {
+        static char[,] board =
+        {
+            {'1','2','3'},{'4','5','6'},{'7','8','9'}
+        };
+    }
+}
