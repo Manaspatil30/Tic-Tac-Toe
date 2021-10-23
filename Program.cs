62996a76f41e9e358e5bc44a5d57c06f7bc641dc
unknown <manaspatil2030@gmail.com>
2021-10-23 09:52:34 +0530

    Added Main Method for tic tac toe

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_CreatBoard uc1 = new UC1_CreatBoard();
+            uc1.Board();
+        }
+    }
+}
